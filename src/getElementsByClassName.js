8907382a64ca1a869ccecee704be30eca0d8c91c
Jacky Pan <Jackypan301@gmail.com>
2020-10-05 15:45:45 -0700

    adding information

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,43 +4,26 @@
 // };
 
 // But instead we're going to implement it from scratch:
-// var getElementsByClassName = function(className
-// ) {
-//   var results = [];
-//   var checkNode = function (node) {
-//     var elementClasses = node.classList;
-//     if (elementClasses.contains(className)) {
-//       results.push(node);
-
-//     }
-//     if (node.childNodes) {
-//       for (var i = 0; i < node.children.length; i++) {
-//         checkNode(node.children[i]);
-//       }
-//     }
-
-//   };
-
-
-//   checkNode(document.body);
-//   return results;
-// };
-
-
-
 var getElementsByClassName = function(className
 ) {
   var results = [];
   var checkNode = function (node) {
-    if (node.classList.contains(className) ) {
+    var elementClasses = node.classList;
+    if (elementClasses.contains(className)) {
       results.push(node);
+
     }
-    for (var i = 0; i < node.children.length; i++) {
-      checkNode(node.children[i]);
+    if (node.childNodes) {
+      for (var i = 0; i < node.children.length; i++) {
+        checkNode(node.children[i]);
+      }
     }
 
   };
 
+
   checkNode(document.body);
   return results;
 };
+
+
