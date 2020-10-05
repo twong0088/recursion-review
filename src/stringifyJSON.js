66c514cc19e696a3b03f86d4062194df9ee84c69
Terrence Wong <terrencewong223@gmail.com>
2020-10-05 15:38:42 -0700

    added functions for stringify and changed specs

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,33 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else if (obj === null) {
+    return 'null';
+  } else if (Array.isArray(obj)) {
+    var arr = [];
+    for (var i = 0; i < obj.length; i++) {
+      arr.push(stringifyJSON(obj[i]));
+    }
+    return '[' + arr.join(',') + ']';
+  } else if (typeof obj === 'object') {
+    var objArr = [];
+    for (var key in obj) {
+      if (typeof obj[key] === 'function' || obj[key] === undefined) {
+        continue;
+      }
+      objArr.push('"' + key + '":' + stringifyJSON(obj[key]));
+    }
+    if (objArr.length === 0 ) {
+      return '{}';
+    } else {
+      return '{' + objArr.join(',') + '}';
+    }
+
+  } else {
+    return '' + obj;
+  }
+
 };
